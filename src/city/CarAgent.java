29e4b3a49a9423aae62d1acd99f7581e2636cd57
Ben Tan <twaih92@gmail.com>
2013-11-12 10:48:13 -0800

    added CarAgent

--- src/city/CarAgent.java
+++ src/city/CarAgent.java
@@ -1,7 +1,91 @@
 package city;
 
 import city.interfaces.Car;
+import city.interfaces.Person;
+import city.interfaces.Transportation;
+import agent.Agent;
 
-public class CarAgent implements Car {
+import java.util.*;
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CarAgent extends Agent implements Car {
+	
+    /**
+	*Data
+	*/
+	private Transportation passenger;
+	
+	public enum carState
+	{inTransit, atDestination, Idle}
+	public carState currentState = carState.Idle;
+	
+	private String destination;
+	private Semaphore driving = new Semaphore(0,true);
+		
+	/**
+	*Scheduler
+	*/	
+		protected boolean pickAndExecuteAnAction(){
+			if (currentState == carState.inTransit){
+				goTo(destination);
+				return true;
+			}
+		
+			else if (currentState == carState.atDestination){
+				parkCar();
+				return true;
+			}
+
+			else if (currentState == carState.Idle){
+				return true;//do nothing
+			}
+			return false;
+		}
+		
+	/**
+	 * Messages
+	 * @param myDestination
+	 */
+		public void msgTakeMeHere(String myDestination){ //receives msg from passenger
+			currentState = carState.inTransit;
+			destination= myDestination;
+			stateChanged();
+		}
+		
+		public void msgAtDestination(){ //from carGui when reached destination
+			driving.release();
+			currentState= carState.atDestination;
+			stateChanged();
+		}
+
+	/**
+	 * Actions	
+	 * @param myDestination
+	 */
+		private void goTo(String myDestination){
+			doGoTo(myDestination); //sets destination in carGui
+			try {
+				driving.acquire(); //to ensure that the gui is uninterrupted on the way
+				} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+		}
+		
+		private void doGoTo(String myDestination){
+			System.out.println("car is going to "+ myDestination);
+			//haven't implemented carGui
+			//carGui.msgGoTo(myDestination);
+		}
+		
+		private void parkCar(){
+		//msg gives passenger the destination so its gui can reappear at an appropriate place
+			passenger.msgArrivedAtDestination(destination);
+			currentState = carState.Idle;
+		}
+
 
 }
--- src/city/TransportationRole.java
+++ src/city/TransportationRole.java
@@ -1,7 +1,47 @@
 package city;
 
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.concurrent.Semaphore;
+
+import agent.Agent;
 import city.interfaces.Transportation;
 
-public class TransportationRole implements Transportation {
+public class TransportationRole extends Agent implements Transportation  {
+        int currentStopNumber = 0;
+        int desiredStopNumber = 0;
+        String destination;
+        CarAgent car;
+        BusAgent bus;
+        Boolean hasCar = false;
+        
+        public enum TransportationState 
+                {Walking, NeedsToTravel, InTransit, AtDestination, None};
+                
+        TransportationState state = TransportationState.None;
 
+        
+        public TransportationRole() {
+                super();
+        }
+        
+        void msgThisIsBusStop(int BusStopNumber) {
+                //bus
+        }
+        
+        void msgArrivedAtDestination(TransportationRole person, String destination) {
+                
+                state = TransportationState.AtDestination;
+                stateChanged();
+        }
+
+
+
+        @Override
+        protected boolean pickAndExecuteAnAction() {
+                // TODO Auto-generated method stub
+                return false;
+        }
 }
