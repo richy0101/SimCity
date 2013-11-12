6576b8475542b144cc5742166df6e214cb4cc798
ryancstack <ryancstack@me.com>
2013-11-12 09:24:57 -0800

    adding role agent

--- src/agent/Role.java
+++ src/agent/Role.java
@@ -1,5 +1,16 @@
 package agent;
 
 public class Role {
+	Person myPerson;
+	public setPerson(Person person) {
+		myPerson = person; 
+	}
+	
+	public PersonAgent getPersonAgent() {
+		return myPerson;
+	}
+	private void stateChanged() {
+		myPerson.stateChanged;
+	}
 
 }
