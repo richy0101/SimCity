15e3e50079393db852f520c861d357f6cdedea65
Reid Nakamura <rknakamu@usc.edu>
2013-11-17 13:28:52 -0800

    created clock singleton

--- src/city/helpers/Clock.java
+++ src/city/helpers/Clock.java
@@ -1,6 +1,19 @@
 package city.helpers;
 
 public class Clock {
+	private static long startTime;
+	static final Clock sharedInstance = new Clock();
 	
+	private Clock() {
+		startTime = System.currentTimeMillis();
+	}
+	
+	public static Clock sharedInstance() {
+		return sharedInstance;
+	}
+	
+	public long getTime() {
+		return System.currentTimeMillis() - startTime;
+	}
 
 }
