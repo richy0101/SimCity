31c4998cf87bf279133db58505d1271c81c2a3de
Reid Nakamura <rknakamu@usc.edu>
2013-11-17 14:50:07 -0800

    filled in market and marketcustomer interfaces

--- src/market/interfaces/Market.java
+++ src/market/interfaces/Market.java
@@ -1,5 +1,9 @@
 package market.interfaces;
 
 public interface Market {
-
+	public void msgGetGroceries(MarketCustomer customer, Map<String, Integer> groceryList);
+	
+	public void msgHereIsMoney(MarketCustomer customer, double money);
+	
+	public void msgCantAffordGroceries(MarketCustomer customer);
 }
