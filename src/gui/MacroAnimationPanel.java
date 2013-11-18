09d3d4093cac7716da85d55d3f1b8236c9debd15
Ryan Sheh <sheh_ryan@guest-wireless-upc-nat-206-117-88-071.usc.edu>
2013-11-18 14:08:20 -0800

    Added basic image to make coordinates are aligned

--- src/gui/MacroAnimationPanel.java
+++ src/gui/MacroAnimationPanel.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -19,16 +20,20 @@ public class MacroAnimationPanel extends JPanel implements ActionListener {
     private final int DELAY = 5;
 
     private List<Gui> guis = new ArrayList<Gui>();
+    private ImageIcon background = new ImageIcon("/team02/src/gui/SIMCITY.jpg");
+    //private ImageIcon background = new ImageIcon("/images/SIMCITY.png");
+    private Image image = background.getImage();
+
 
     public MacroAnimationPanel() {
     	setSize(WINDOWX, WINDOWY);
         setVisible(true);
-        setBackground(Color.lightGray);
- 
+        setBackground(Color.darkGray);
+        
     	Timer timer = new Timer(DELAY, this);
     	timer.start();
     }
-
+    
 	public void actionPerformed(ActionEvent e) {
 		repaint();  //Will have paintComponent called
 	}
@@ -39,6 +44,7 @@ public class MacroAnimationPanel extends JPanel implements ActionListener {
         //Clear the screen by painting a rectangle the size of the frame
         g2.setColor(getBackground());
         g2.fillRect(0, 0, getWidth(), getHeight());
+        //g2.drawImage(image, 0, 0, getWidth(), getHeight(), this);
 
         for(Gui gui : guis) {
             if (gui.isPresent()) {
