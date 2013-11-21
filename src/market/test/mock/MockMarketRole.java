79f737fe5ceba966e344f696c022ce4234f7450b
Reid Nakamura <rknakamu@usc.edu>
2013-11-20 18:32:01 -0800

    Added market unit test files

---
+++ src/market/test/mock/MockMarketRole.java
@@ -0,0 +1,15 @@
+package market.test.mock;
+
+import market.interfaces.Market;
+
+
+/**
+ * A sample MockCustomer built to unit test a CashierAgent.
+ *
+ * @author Monroe Ekilah
+ *
+ */
+public class MockMarketRole extends Mock implements Market {
+
+
+}
