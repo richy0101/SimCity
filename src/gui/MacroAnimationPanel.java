248a8319abecf086637fa7f2f30169804e7c123f
Ryan Sheh <sheh_ryan@Ryan-Shehs-MacBook-Pro.local>
2013-11-23 00:34:22 -0800

    Pretty much completed the coding of Macro, just need to implement it correctly

--- src/gui/MacroAnimationPanel.java
+++ src/gui/MacroAnimationPanel.java
@@ -7,6 +7,8 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,7 +17,7 @@ import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class MacroAnimationPanel extends JPanel implements ActionListener {
+public class MacroAnimationPanel extends JPanel implements ActionListener, MouseListener {
        
 	BufferedImage cityImage;
 	BufferedImage cityImageTop;
@@ -28,6 +30,7 @@ public class MacroAnimationPanel extends JPanel implements ActionListener {
     private final int DELAY = 5;
 
     private List<Gui> guis = new ArrayList<Gui>();
+    ArrayList<Building> buildings;
 
     public MacroAnimationPanel() {
     	setSize(WINDOWX, WINDOWY);
@@ -37,6 +40,15 @@ public class MacroAnimationPanel extends JPanel implements ActionListener {
     	Timer timer = new Timer(DELAY, this);
     	timer.start();
     	
+    	buildings = new ArrayList<Building>();
+    	
+    	//Adding building blocks
+    	Building b = new Building(0, 0, 10, 10);
+    	buildings.add(b);
+    	
+    	addMouseListener(this);
+    	
+    	//CITY BACKGROUND
     	try {
         	cityImage = ImageIO.read(getClass().getResource("SIMCITYBOTTOM.png"));
         	cityImageTop = ImageIO.read(getClass().getResource("SIMCITYTOP.png"));
@@ -55,6 +67,13 @@ public class MacroAnimationPanel extends JPanel implements ActionListener {
 
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.black);
+        
+        //paint all building squares black
+        for(int i = 0; i < buildings.size(); i++) {
+        	Building b = buildings.get(i);
+        	g2.fill(b);
+        }
 
         g2.drawImage(cityImage, 0, 0, null);
         
@@ -73,8 +92,46 @@ public class MacroAnimationPanel extends JPanel implements ActionListener {
         g2.drawImage(cityImageTop, 0, 0, null);
         
     }
+    
+    public ArrayList<Building> getBuildings() {
+    	return buildings;
+    }
 
     public void addGui(Gui gui) {
         guis.add(gui);
     }
+
+    //if click on building, then building displayed
+	public void mouseClicked(MouseEvent e) {
+		for(int i = 0; i < buildings.size(); i++) {
+			Building b= buildings.get(i);
+			if(b.contains(e.getX(), e.getY())) {
+				b.displayBuilding();
+			}
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
