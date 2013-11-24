1f679a25760919ec405ec7d7834621da5c52984c
Ryan Sheh <sheh_ryan@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-24 15:52:00 -0800

    Added all the references to the buildings as cards in the microanimationpanel

--- src/gui/MicroAnimationPanel.java
+++ src/gui/MicroAnimationPanel.java
@@ -50,8 +50,25 @@ public class MicroAnimationPanel extends JPanel implements ActionListener, Mouse
     	timer.start();
     	
     	cards.put("null", new CityCard(myCity, Color.blue));
+    	
     	cards.put("stackRestaurant", new CityCard(myCity, Color.pink));
+    	cards.put("huangRestaurant", new CityCard(myCity, Color.pink));
+    	cards.put("phillipsRestaurant", new CityCard(myCity, Color.pink));
+    	cards.put("nakamuraRestaurant", new CityCard(myCity, Color.pink));
+    	cards.put("shehRestaurant", new CityCard(myCity, Color.pink));
+    	cards.put("tanRestaurant", new CityCard(myCity, Color.pink));
+    	
+    	cards.put("house1", new CityCard(myCity, Color.pink));
+    	cards.put("house2", new CityCard(myCity, Color.pink));
+    	cards.put("house3", new CityCard(myCity, Color.pink));
+    	cards.put("house4", new CityCard(myCity, Color.pink));
+    	cards.put("house5", new CityCard(myCity, Color.pink));
+    	cards.put("house6", new CityCard(myCity, Color.pink));
+    	
     	cards.put("market1", new CityCard(myCity, Color.yellow));
+    	cards.put("market2", new CityCard(myCity, Color.yellow));
+    	
+    	cards.put("bank", new CityCard(myCity, Color.yellow));
     	
     	layout = new CardLayout();
     	this.setLayout(layout);
