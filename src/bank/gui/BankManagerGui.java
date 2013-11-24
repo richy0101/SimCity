50261b0e2966ea7f57d4373772bf7593fb9b5188
Richard Phillips <richy0101@gmail.com>
2013-11-23 18:15:25 -0800

    Added bankmanagergui first iteration

--- src/bank/gui/BankManagerGui.java
+++ src/bank/gui/BankManagerGui.java
@@ -4,24 +4,36 @@ import gui.Gui;
 
 import java.awt.Graphics2D;
 
-public class BankManagerGui implements Gui {
+import bank.*;
 
+
+public class BankManagerGui implements Gui {
+	
+    
+	private BankManagerRole agent = null;
+    
+	public int xPos = 50, yPos = 200;//default manager position
+    
+	public BankManagerGui(BankManagerRole agent) {
+        this.agent = agent;
+	}
+	
 	@Override
 	public void updatePosition() {
 		// TODO Auto-generated method stub
-
+        
 	}
-
+    
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-
+		// assign custom pokemon graphics to manager
+        
 	}
-
+    
 	@Override
 	public boolean isPresent() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+    
 }
