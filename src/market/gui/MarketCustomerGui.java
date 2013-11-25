1a468829c54b1f453e4293d5e7fe2f484b47bbaa
Ryan Sheh <sheh_ryan@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-24 21:30:13 -0800

    Implementing market gui

--- src/market/gui/MarketCustomerGui.java
+++ src/market/gui/MarketCustomerGui.java
@@ -20,10 +20,10 @@ public class MarketCustomerGui implements Gui {
 	private enum state {NoCommand, Entering, Leaving};
 	private state command = state.NoCommand;
 	
-	public static final int xCounter = 75;
-	public static final int yCounter = 50;
-	public static final int xStart = -40;
-	public static final int yStart = -40;
+	public static final int xCounter = 103;
+	public static final int yCounter = 346;
+	public static final int xStart = 833;
+	public static final int yStart = 359;
 	
 	
 	public MarketCustomerGui(MarketCustomerRole mcr) {
