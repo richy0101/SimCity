56a6bf979e07d919eb5b75e38d47d3cea56f006d
Ryan Sheh <sheh_ryan@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-25 22:13:07 -0800

    added mockperson

---
+++ src/bank/test/mock/MockPerson.java
@@ -0,0 +1,81 @@
+package bank.test.mock;
+
+import java.util.Map;
+
+import city.interfaces.Person;
+import bank.interfaces.BankTeller;
+import bank.interfaces.BankCustomer;
+import bank.interfaces.BankManager;
+
+public class MockPerson extends Mock implements Person {
+
+	public BankTeller teller;
+	public BankManager manager;
+	public BankCustomer customer;
+	
+	public EventLog log;
+	public Map<String, Integer> groceryList;
+	public double funds;
+
+	public MockPerson(String name) {
+		super(name);
+		log = new EventLog();
+	}
+
+	@Override
+	public void clearGroceries() {
+		log.add(new LoggedEvent("ClearGroceries called"));
+		groceryList.clear();
+	}
+
+	@Override
+	public double getFunds() {
+		log.add(new LoggedEvent("GetFunds called"));
+		return funds;
+	}
+
+	@Override
+	public void setFunds(double funds) {
+		log.add(new LoggedEvent("SetFunds called. Amount = $" + funds));
+		this.funds = funds;		
+	}
+	
+	@Override
+	public void msgRoleFinished() {
+		log.add(new LoggedEvent("msgRoleFinished called"));
+	}
+
+	@Override
+	public void stateChanged() {
+	}
+
+	@Override
+	public void print(String msg) {
+	}
+
+	@Override
+	public void Do(String msg) {
+	}
+
+	@Override
+	public String getTransportationMethod() {
+		return null;
+	}
+
+	@Override
+	public void setAccountNumber(int accountNumber) {
+		
+	}
+
+	@Override
+	public int getAccountNumber() {
+		return 0;
+	}
+
+	@Override
+	public void msgTransportFinished(String currentLocation) {
+		
+	}
+	
+
+}
