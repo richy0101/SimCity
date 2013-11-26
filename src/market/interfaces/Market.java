6be52e6c239e71790a7fa09cf6f48ee1dcf96e13
ryancstack <ryancstack@me.com>
2013-11-26 09:13:45 -0800

    finished integration of market and restaurant

--- src/market/interfaces/Market.java
+++ src/market/interfaces/Market.java
@@ -2,10 +2,17 @@ package market.interfaces;
 
 import java.util.Map;
 
+import restaurant.stackRestaurant.interfaces.Cashier;
+import restaurant.stackRestaurant.interfaces.Cook;
+
 public interface Market {
 	public void msgGetGroceries(MarketCustomer customer, Map<String, Integer> groceryList);
 	
 	public void msgHereIsMoney(MarketCustomer customer, double money);
 	
 	public void msgCantAffordGroceries(MarketCustomer customer);
+	
+	public void msgOrderFood(Cook cook, Cashier cashier, String choice);
+	
+	public void msgPayForOrder(Cashier cashier, double funds);
 }
--- src/restaurant/stackRestaurant/interfaces/Market.java
+++
@@ -1,9 +0,0 @@
-package restaurant.stackRestaurant.interfaces;
-
-public interface Market {
-	
-	public abstract void msgOrderFood(Cook cook, String choice);
-	
-	public abstract void msgPayForOrder(double funds);
-
-}
