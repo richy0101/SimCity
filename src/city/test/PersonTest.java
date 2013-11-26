6eea16279bf248474d41f7a546a033a3112e86ac
Ben Tan <twaih92@gmail.com>
2013-11-26 07:16:47 -0800

    added PersonTest file

---
+++ src/city/test/PersonTest.java
@@ -0,0 +1,20 @@
+package city.test;
+
+import junit.framework.*;
+import city.test.mock.MockTransportation;
+import city.PersonAgent;
+
+public class PersonTest extends TestCase {
+	
+	MockTransportation passenger;
+	PersonAgent person;
+	MockTransportation trole;
+
+	public void setUp() throws Exception{
+		super.setUp();	
+		
+		PersonAgent person;
+		MockTransportation trole;
+			
+	}	
+}
