3f5991ffca9c1ddfbd4454cfe6bb6a1c94f08c08
Reid Nakamura <rknakamu@usc.edu>
2013-11-26 19:46:05 -0800

    fixed MockCashier

--- src/restaurant/stackRestaurant/test/mock/MockCashier.java
+++ src/restaurant/stackRestaurant/test/mock/MockCashier.java
@@ -1,5 +1,6 @@
 package restaurant.stackRestaurant.test.mock;
 
+import agent.Role;
 import restaurant.stackRestaurant.helpers.Check;
 import restaurant.stackRestaurant.interfaces.Cashier;
 import restaurant.stackRestaurant.interfaces.Customer;
@@ -22,6 +23,12 @@ public class MockCashier extends Mock implements Cashier {
 	}
 
 	public void msgGiveBill(Check check, Market market) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgNeedPaycheck(Role role) {
 		// TODO Auto-generated method stub
 		
 	}
