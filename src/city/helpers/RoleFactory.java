38763d906fd60e2ce3effc7b475fe1702b80cf6f
Alexander Huang <huangan@usc.edu>
2013-12-08 15:46:42 -0800

    Deleting unesecarry filesss.

--- src/city/helpers/RoleFactory.java
+++ src/city/helpers/RoleFactory.java
@@ -5,7 +5,14 @@ import city.UnemployedRole;
 import market.MarketCustomerRole;
 import agent.Role;
 import bank.BankTellerRole;
-import restaurant.stackRestaurant.*;
+
+import restaurant.huangRestaurant.HuangCookRole;
+import restaurant.huangRestaurant.HuangCustomerRole;
+import restaurant.huangRestaurant.HuangWaiterNormalRole;
+import restaurant.huangRestaurant.HuangWaiterSharedRole;
+import restaurant.stackRestaurant.StackCookRole;
+import restaurant.stackRestaurant.StackWaiterNormalRole;
+import restaurant.stackRestaurant.StackWaiterSharedRole;
 
 public class RoleFactory {
 	Role newRole;
@@ -14,11 +21,23 @@ public class RoleFactory {
 	}
 	public Role createRole(String role, PersonAgent p) {
 		System.out.println(role);
-		if(role.equals("StackRestaurant")) {
-			newRole = new StackCustomerRole("StackRestaurant");
+		if(role.equals("HuangRestaurant")) {
+			newRole = new HuangCustomerRole("HuangRestaurant");
 		}
 		else if(role.equals("Market1") || role.equals("Market2")) {
 			newRole = new MarketCustomerRole(p.getGroceriesList(), role);
+		}
+		else if(role.equals("HuangWaiterNormal")) {
+			newRole = new HuangWaiterNormalRole("HuangRestaurant");
+			return newRole;
+		}
+		else if (role.equals("HuangWaiterShared")) {
+			newRole = new HuangWaiterSharedRole("HuangRestaurant");
+			return newRole;
+		}
+		else if (role.equals("HuangCook")) {
+			newRole = new HuangCookRole("HuangRestaurant");
+			return newRole;
 		}
 		else if(role.equals("StackWaiterNormal")) {
 			newRole = new StackWaiterNormalRole("StackRestaurant");
