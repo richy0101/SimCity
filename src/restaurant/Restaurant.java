8ac4906534623d62525b39aca9be8dd1dfc58840
ryancstack <ryancstack@me.com>
2013-12-08 12:40:29 -0800

    thread safe business

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -1,13 +1,15 @@
 package restaurant;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import restaurant.stackRestaurant.ProducerConsumerMonitor;
 
 public class Restaurant {
 	
 	private boolean isOpen = true;
-	private HashMap<String, FoodData> foodInventory = new HashMap<String, FoodData>();
+	private Map<String, FoodData> foodInventory = Collections.synchronizedMap(new HashMap<String, FoodData>());
 
 	private ProducerConsumerMonitor monitor;
 	public Restaurant() {
@@ -31,19 +33,23 @@ public class Restaurant {
 	}
 	
 	public void msgChangeFoodInventory(String type, int quantity) {
-		
+		foodInventory.get(type).setQuantity(quantity);
 	}
 	
 	public void msgChangeOpen() {
 
 	}
 	
-	public HashMap<String, FoodData> getFoodInventory() {
+	public Map<String, FoodData> getFoodInventory() {
 		return foodInventory;
 	}
 	
 	private class FoodData {
+		int quantity;
 		
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
 	}
 
 }
